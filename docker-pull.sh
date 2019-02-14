fc542562a826fc966a451fa384fef7b2324ab240
Fede Diaz <nordri@gmail.com>
2019-02-14 15:52:54 +0100

    Fixed docker images tags

--- docker-pull.sh
+++ docker-pull.sh
@@ -1,11 +1,11 @@
 docker pull jocatalin/kubernetes-bootcamp:v1
 docker pull jocatalin/kubernetes-bootcamp:v2
 docker pull codeurjc/webgatos:v1
-docker pull byrnedo/alpine-curl
+docker pull byrnedo/alpine-curl:0.1.7
 docker pull gcr.io/google-samples/node-hello:1.0
 docker pull mysql:5.6
 docker pull codeurjc/java-webapp-bbdd:v2
-docker pull k8s.gcr.io/test-webserver
-docker pull nginx
-docker pull k8s.gcr.io/hpa-example
-docker pull busybox
+docker pull codeurjc/test-webserver:v1
+docker pull nginx:1.15
+docker pull codeurjc/hpa-example:v1
+docker pull busybox:1.30
